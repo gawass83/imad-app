43d8f2bf58a8392d4ad74ec7e14e2c267fd919ee
gawass83 <31266674+gawass83@users.noreply.github.com>
2017-09-01 13:01:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,130 +1,25 @@
-var express = require('express');
-var morgan = require('morgan');
-var path = require('path');
+var express=require('express');
+var morgan=require('morgan');
+var path=require('path');
 
-var app = express();
+var app=express();
 app.use(morgan('combined'));
 
-var artone={
-    title: 'Article one | Shubham',
-    heading: 'Article one',
-    date:'Sept 5,2082',
-    content: 
-    `  <p>
-         
-         This the content This the content
-     This the content
-     This the content
-     This the content
-     This the content
-     </p>           
-                
-         <p>
-         
-         This the content
-     </p>           
-        
-         <p>
-         
-         This the content
-     </p>           
-        
-   
-    `
-};
-
-
-function createTemplate(data){
-    var title=data.title;
-    var date=data.date;
-    var heading=data.heading;
-    var content=data.comtent;
-    
-}
-var htmltemplate=
-`
-
-
-<html>
-
-    <head>
-        
-    <title> ${title}</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1"/>
-<style>
-    .container{
-        max-width:800px;
-        margin:0 auto;
-        color:#000000;
-        font-family:calibri;
-        padding-top:80px;
-    }
-</style>
-
-    </head>
-    
-    <body>
-        <div class="container">
-            
-        
-        
-        <div>
-            <a href="/">Home</a>
-        </div>
-        
-        <div>
-            <h3>
-       ${heading}
-</h3>
-        </div>
-        
-        
-        
-        <div>
-${date}
- </div>
-     ${content}
-        
-        </div>
-    </body>
-</html>
-`;
-return htmlTemplate;
-
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+app.get('/'.function(req,res))
+res.sendFile(path.join(__dirname,'ui','index.html'));
 });
 
-app.get('/articleone', function (req, res) {
-  res.send(createTemplate(articleone())
-  
+app.get('/ui/style.css'.function(req,res))
+res.sendFile(path.join(__dirname,'ui','style.css'));
 });
 
-app.get('/articletwo',function(req,res){
-    res.send('article two will ne rqested here');
-});
-
-app.get('/articlethree',function(req,res){
-    res.send('article three will be requested here');
-    
+app.get('/ui/madi.png'.function(req,res))
+res.sendFile(path.join(__dirname,'ui','madi.png'));
 });
 
 
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'gs.png'));
-});
-
-
-// Do not change port, otherwise your app won't run on IMAD servers
-// Use 8080 only for local development if you already have apache running on 80
-
-var port = 80;
-app.listen(port, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
+var port=8080;
+app.listen(8080,function()
+{
+    console.log('IMAD course app listenig on port ${port}');
 });
